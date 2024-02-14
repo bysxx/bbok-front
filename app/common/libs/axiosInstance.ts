5a498fab7ba09eaf46949b871af87eb3fef225f9
suminhan123 <suminh324@gmail.com>
2024-02-14 19:31:42 +0900

    feat: add httpClient interface

--- app/common/libs/axiosInstance.ts
+++ app/common/libs/axiosInstance.ts
@@ -1,9 +1,19 @@
 import { BASE_URL } from '@constants/url';
-import type { AxiosInstance } from 'axios';
+import type { AxiosInstance, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
+
+export interface HttpClient extends AxiosInstance {
+  get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  post<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  put<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  patch<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  delete<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+}
+
+export const http: HttpClient = axiosInstance;
 
 axiosInstance.interceptors.response.use(
   (res) => {
