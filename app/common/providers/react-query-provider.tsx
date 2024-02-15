07781e82a5a98601230131c0faedb9464b246b2a
suminhan123 <suminh324@gmail.com>
2024-02-15 20:15:56 +0900

    chore: remove react-query

--- app/common/providers/react-query-provider.tsx
+++ app/common/providers/react-query-provider.tsx
@@ -1,18 +1,12 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 
 function ReactQueryProvider({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
 
 export default ReactQueryProvider;
