c37f6db96c4261e0eccfd722a663a548c797a83b
suminhan123 <suminh324@gmail.com>
2024-02-15 16:14:32 +0900

    feat: save login token to cookie

--- app/kakao/page.tsx
+++ app/kakao/page.tsx
@@ -4,6 +4,7 @@ import Loading from '@components/ui/loading';
 import { redirectUri } from '@libs/config';
 import authApi from '@requests/auth';
 import { useUserStore } from '@stores/useUserStore';
+import { setCookie } from 'cookies-next';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -18,11 +19,12 @@ export default function KakaoPage() {
       authApi.signIn(searchParams.get('code') as string, redirectUri).then((res) => {
         setUserData(res.data);
         // 일단 쿠키에 저장
-
+        setCookie('accessToken', res.data.accessToken);
+        setCookie('refreshToken', res.data.refreshToken);
         // 처음 사용하는 유저이면
-        router.replace('/checklist');
+        // router.replace('/checklist');
         // 이전에도 사용한 유저이면
-        // router.replace('/');
+        router.replace('/');
       });
     }
   }, [searchParams]);
