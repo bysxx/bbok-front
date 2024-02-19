86c4cbf6be98b1aa5440b3590974ab3082569296
suminhan123 <suminh324@gmail.com>
2024-02-19 23:39:50 +0900

    feat: add friend type to api func

--- app/common/hooks/queries/friend/query.ts
+++ app/common/hooks/queries/friend/query.ts
@@ -1,9 +1,12 @@
 import { FRIEND_KEYS } from '@constants/queryKeys';
+import { IFriendList } from '@interfaces/friend';
+import { ResponseDTO } from '@requests/common';
 import friendApi from '@requests/friend/friend.client';
 import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 export const useGetFriend = () => {
-  return useQuery({
+  return useQuery<ResponseDTO<IFriendList>, AxiosError>({
     queryKey: FRIEND_KEYS.lists(),
     queryFn: friendApi.get,
   });
