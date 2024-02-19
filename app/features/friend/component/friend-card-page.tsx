716cc2a40735f1836e6e1e845c2023e01103ffca
suminhan123 <suminh324@gmail.com>
2024-02-19 23:48:24 +0900

    refactor: refactor empty friend component

--- app/features/friend/component/friend-card-page.tsx
+++ app/features/friend/component/friend-card-page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { RoundButton } from '@components/buttons';
 import BoxButton from '@components/buttons/box-button';
 import { DiaryTopBar } from '@components/top-bar';
 import { useGetFriend } from '@hooks/queries/friend';
 import FriendCard from 'app/(home)/friends/friend-card';
 import LockedFriendCard from 'app/(home)/friends/locked-friend-card';
+
+import EmptyFriend from './empty-friend';
 
 const FriendCardPage = () => {
   const { data } = useGetFriend();
@@ -16,17 +17,8 @@ const FriendCardPage = () => {
       {(() => {
         if (data?.data.friends.length === 0) {
           return (
-            <div className="mx-9 mt-9 rounded-3xl bg-white pb-[53px] pt-[51px]">
-              <div className="flex flex-col items-center justify-center">
-                <img src={'/images/illustration/empty-state-kaka.svg'} alt="" />
-                <h1 className="mt-5 text-base font-bold text-gray-65">등록된 친구가 아직 없어요!</h1>
-                <h5 className="text-caption-1 mt-3 flex items-center justify-center text-gray-40">
-                  친구를 등록하고 일화를 작성하여
-                </h5>
-                <h5 className="text-caption-1 mb-[17px] text-gray-40">생각을 정리해 보세요</h5>
-
-                <RoundButton type="secondary" onClick={() => {}} label="친구 생성" />
-              </div>
+            <div className="mx-9 mt-9">
+              <EmptyFriend />
             </div>
           );
         }
---
+++ app/features/friend/component/index.ts
@@ -0,0 +1,2 @@
+export { default as EmptyFriend } from './empty-friend';
+export { default as FriendCardPage } from './friend-card-page';
