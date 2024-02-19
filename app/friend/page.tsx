8592959e8a17afa9729fe8860990d8a320da4895
suminhan123 <suminh324@gmail.com>
2024-02-20 00:51:49 +0900

    feat: apply prefetch hydration to friend make page

--- app/friend/page.tsx
+++ app/friend/page.tsx
@@ -1,6 +1,13 @@
+import { PrefetchHydration } from '@components/react-query';
+import { FRIEND_KEYS } from '@constants/queryKeys';
 import { FriendMakePage } from '@features/friend/component';
+import friendServerApi from '@requests/friend/friend.server';
 
 const FriendPage = () => {
-  return <FriendMakePage />;
+  return (
+    <PrefetchHydration queryKey={FRIEND_KEYS.details()} queryFn={friendServerApi.character}>
+      <FriendMakePage />
+    </PrefetchHydration>
+  );
 };
 export default FriendPage;
