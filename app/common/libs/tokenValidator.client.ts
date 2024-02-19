89ff14ce02a2bfef5955828f03b77c689d31ce74
suminhan123 <suminh324@gmail.com>
2024-02-19 22:20:59 +0900

    feat: add client, server tokenvalidator func

---
+++ app/common/libs/tokenValidator.client.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-cycle
+import authApi from '@requests/auth';
+import { setCookie } from 'cookies-next';
+
+export const getAccessTokenClient = async (refreshToken: string): Promise<null | string> => {
+  try {
+    if (refreshToken) {
+      // token refresh 로직 처리
+      const data = await authApi.reissue(refreshToken);
+      setCookie('accessToken', data.data.accessToken);
+      setCookie('refreshToken', data.data.refreshToken);
+
+      return data.data.accessToken;
+    }
+    return null;
+  } catch (e) {
+    // TODO: 에러 타입 지정
+    // return e as ApiError;
+    return null;
+  }
+};
