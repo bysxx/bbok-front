80eb533c0f8c0cf2935979f808f93cb8e6e3c364
suminhan123 <suminh324@gmail.com>
2024-02-20 01:10:20 +0900

    feat: add friend charater api hook func

--- app/common/hooks/queries/friend/query.ts
+++ app/common/hooks/queries/friend/query.ts
@@ -1,5 +1,5 @@
 import { FRIEND_KEYS } from '@constants/queryKeys';
-import { IFriendList } from '@interfaces/friend';
+import { ICharacterImage, IFriendList } from '@interfaces/friend';
 import { ResponseDTO } from '@requests/common';
 import friendApi from '@requests/friend/friend.client';
 import { useQuery } from '@tanstack/react-query';
@@ -11,3 +11,10 @@ export const useGetFriend = () => {
     queryFn: friendApi.get,
   });
 };
+
+export const useGetFriendCharater = () => {
+  return useQuery<ResponseDTO<ICharacterImage>, AxiosError>({
+    queryKey: FRIEND_KEYS.details(),
+    queryFn: friendApi.character
+  })
+}
