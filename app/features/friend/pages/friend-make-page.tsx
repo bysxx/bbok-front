215c4e75dd99f870f4eb96e1057538b43b405fa5
suminhan123 <suminh324@gmail.com>
2024-02-20 14:53:22 +0900

    feat: apply character get api to friend page

--- app/features/friend/pages/friend-make-page.tsx
+++ app/features/friend/pages/friend-make-page.tsx
@@ -4,34 +4,20 @@ import Input from '@components/input';
 import { ButtonTopBar } from '@components/top-bar';
 import DefaultLayout from '@components/ui/layout/default-layout';
 import FooterButtonLayout from '@components/ui/layout/footer-button-layout';
-
 import { useGetFriendCharater } from '@hooks/queries/friend';
 import useCustomRouter from '@hooks/useCustomRouter';
 import useInput from '@hooks/Utils/useInput';
-import { ICharacter } from '@interfaces/friend';
+import type { TFriendCharacter } from '@interfaces/friend';
 import FriendCharacter from 'app/friend/friend-character';
+import { useState } from 'react';
 
-import { useState, useCallback } from 'react';
 import { friendInputVerifier } from '../utils/friendInputVerifier';
-
-
 
 const FriendMakePage = () => {
   const { data } = useGetFriendCharater();
-  const { text: value, isValid: error, onChange} = useInput('', friendInputVerifier);
-  const {push} = useCustomRouter();
-  const characterList: ICharacter[] = ['kaka', 'sisi'];
-  // 선택한 캐릭터
-  const [character, setCharacter] = useState<ICharacter>('kaka');
-  // 작성한 친구 이름
-  const [name, setName] = useState<string>('');
-
-  const handleCharacterChange = useCallback(
-    (data: ICharacter) => {
-      setCharacter(data);
-    },
-    [setCharacter],
-  );
+  const { text: name, isValid: error, onChange } = useInput('', friendInputVerifier);
+  const { push } = useCustomRouter();
+  const [character, setCharacter] = useState<TFriendCharacter>('CACTUS');
 
   const handleFriendMake = () => {
     const body = {
@@ -41,12 +27,7 @@ const FriendMakePage = () => {
     console.log(body);
   };
   return (
-    <FooterButtonLayout
-      text="완료"
-      border={false}
-      disabled={!error}
-      onClick={handleFriendMake}
-    >
+    <FooterButtonLayout text="완료" border={false} disabled={!error} onClick={handleFriendMake}>
       <ButtonTopBar
         label={'친구생성'}
         name={'닫기'}
@@ -57,19 +38,27 @@ const FriendMakePage = () => {
       <DefaultLayout>
         <h2 className="mb-4 text-[17px] font-medium text-gray-65">캐릭터 선택</h2>
         <div className="flex items-center">
-          {characterList.map((c) => (
-            <FriendCharacter
-              key={c}
-              selected={character === c}
-              character={character}
-              setCharacter={handleCharacterChange}
-              type={c}
-            />
-          ))}
+          {data?.data.characters &&
+            data?.data.characters.map((friends) => (
+              <FriendCharacter
+                key={friends.type}
+                selected={character === friends.type}
+                character={character}
+                setCharacter={setCharacter}
+                type={friends.type}
+                name={friends.name}
+              />
+            ))}
         </div>
         <h2 className="mb-4 mt-8 text-[17px] font-medium text-gray-65">친구 이름</h2>
-        <Input inputValue={value} setInputValue={onChange} error={!error} placeholder='이름을 입력하세요' maxLength={12} errorMessage='한글 또는 영문,숫자의 조합으로 12자 이내' />
-
+        <Input
+          inputValue={name}
+          setInputValue={onChange}
+          error={!error}
+          placeholder="이름을 입력하세요"
+          maxLength={12}
+          errorMessage="한글 또는 영문,숫자의 조합으로 12자 이내"
+        />
       </DefaultLayout>
     </FooterButtonLayout>
   );
--- app/friend/friend-character.tsx
+++ app/friend/friend-character.tsx
@@ -1,36 +1,24 @@
 import RadioButton from '@components/radio-button';
-import type { ICharacter } from '@interfaces/friend';
+import { FRIEND_CHARACTER } from '@constants/friend';
+import type { TFriendCharacter } from '@interfaces/friend';
 import { useCallback } from 'react';
 
 interface CharacterProps {
-  type: ICharacter;
+  type: TFriendCharacter;
   selected: boolean;
-  character: ICharacter;
-  setCharacter: (value: ICharacter) => void;
+  character: TFriendCharacter;
+  setCharacter: (value: TFriendCharacter) => void;
+  name: string;
 }
 
-const FriendCharacter = ({ type, character, setCharacter, selected }: CharacterProps) => {
-  const characters = {
-    kaka: {
-      selected: '/images/illustration/small-selected-kaka.svg',
-      default: '/images/illustration/small-default-kaka.svg',
-      title: '카카',
-      label: '매일이 짜증나는 카카',
-    },
-    sisi: {
-      selected: '/images/illustration/small-selected-sisi.svg',
-      default: '/images/illustration/small-default-sisi.svg',
-      title: '시시',
-      label: '맑은 눈의 광캐',
-    },
-  };
+const FriendCharacter = ({ type, character, setCharacter, selected, name }: CharacterProps) => {
   const handleSelectedClick = useCallback(() => {
-    setCharacter(character === 'kaka' ? 'sisi' : 'kaka');
+    setCharacter(character === 'CACTUS' ? 'HEDGEHOG' : 'CACTUS');
   }, [setCharacter, character]);
 
   return (
     <div
-      className={`relative flex h-36 w-36 flex-col rounded-xl ${
+      className={`relative flex size-36 flex-col rounded-xl ${
         selected ? 'border-2 border-orange-1 bg-[#FFDFCA]' : 'bg-[#E0E0E0]'
       } mr-[10px]`}
     >
@@ -38,9 +26,13 @@ const FriendCharacter = ({ type, character, setCharacter, selected }: CharacterP
         <RadioButton isChecked={selected} setIsChecked={handleSelectedClick} />
       </div>
       <div className="flex h-full flex-col items-center">
-        <img className="mt-3" src={selected ? characters[type].selected : characters[type].default} alt="" />
-        <h4 className="mt-[5px] text-sm font-medium text-gray-65">{characters[type].title}</h4>
-        <h5 className="text-caption-2 mt-[5px] text-center text-gray-55">{characters[type].label}</h5>
+        <img
+          className="mt-3"
+          src={selected ? FRIEND_CHARACTER[type].selected : FRIEND_CHARACTER[type].default}
+          alt=""
+        />
+        <h4 className="mt-[5px] text-sm font-medium text-gray-65">{name}</h4>
+        <h5 className="text-caption-2 mt-[5px] text-center text-gray-55">{FRIEND_CHARACTER[type].label}</h5>
       </div>
     </div>
   );
