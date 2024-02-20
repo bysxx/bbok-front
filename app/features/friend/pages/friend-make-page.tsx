663a7d954385146ca42a3934a3620d59ae7b9873
suminhan123 <suminh324@gmail.com>
2024-02-20 13:19:47 +0900

    feat: apply custom input verifier component to friend make page

--- app/features/friend/pages/friend-make-page.tsx
+++ app/features/friend/pages/friend-make-page.tsx
@@ -4,34 +4,27 @@ import Input from '@components/input';
 import { ButtonTopBar } from '@components/top-bar';
 import DefaultLayout from '@components/ui/layout/default-layout';
 import FooterButtonLayout from '@components/ui/layout/footer-button-layout';
+
 import { useGetFriendCharater } from '@hooks/queries/friend';
 import useCustomRouter from '@hooks/useCustomRouter';
+import useInput from '@hooks/Utils/useInput';
 import { ICharacter } from '@interfaces/friend';
 import FriendCharacter from 'app/friend/friend-character';
 
+import { useState, useCallback } from 'react';
+import { friendInputVerifier } from '../utils/friendInputVerifier';
 
-import { useState, useMemo, useCallback } from 'react';
+
 
 const FriendMakePage = () => {
   const { data } = useGetFriendCharater();
-  console.log(data);
+  const { text: value, isValid: error, onChange} = useInput('', friendInputVerifier);
   const {push} = useCustomRouter();
   const characterList: ICharacter[] = ['kaka', 'sisi'];
   // 선택한 캐릭터
   const [character, setCharacter] = useState<ICharacter>('kaka');
   // 작성한 친구 이름
   const [name, setName] = useState<string>('');
-  const [nameState, setNameState] = useState<'empty' | 'success' | 'error'>('empty');
-
-  useMemo(() => {
-    if (name.length === 0) {
-      setNameState('empty');
-    } else if (name.length > 0 && name.length < 12) {
-      setNameState('success');
-    } else {
-      setNameState('error');
-    }
-  }, [name]);
 
   const handleCharacterChange = useCallback(
     (data: ICharacter) => {
@@ -39,21 +32,6 @@ const FriendMakePage = () => {
     },
     [setCharacter],
   );
-
-  const alertText = {
-    empty: {
-      image: '/images/icon/ui/alert-gray.svg',
-      color: 'text-gray-25',
-    },
-    success: {
-      image: '/images/icon/ui/alert-success.svg',
-      color: 'text-success',
-    },
-    error: {
-      image: '/images/icon/ui/alert-red.svg',
-      color: 'text-alert',
-    },
-  };
 
   const handleFriendMake = () => {
     const body = {
@@ -66,7 +44,7 @@ const FriendMakePage = () => {
     <FooterButtonLayout
       text="완료"
       border={false}
-      disabled={nameState === 'empty' || nameState === 'error'}
+      disabled={!error}
       onClick={handleFriendMake}
     >
       <ButtonTopBar
@@ -90,14 +68,8 @@ const FriendMakePage = () => {
           ))}
         </div>
         <h2 className="mb-4 mt-8 text-[17px] font-medium text-gray-65">친구 이름</h2>
-        <Input inputValue={name} setInputValue={setName} error={nameState === 'error'} />
+        <Input inputValue={value} setInputValue={onChange} error={!error} placeholder='이름을 입력하세요' maxLength={12} errorMessage='한글 또는 영문,숫자의 조합으로 12자 이내' />
 
-        <div className="mt-3 flex items-center">
-          <img src={alertText[nameState].image} alt="" />
-          <h5 className={`text-caption-1 ml-1 ${alertText[nameState].color}`}>
-            한글 또는 영문,숫자의 조합으로 12자 이내
-          </h5>
-        </div>
       </DefaultLayout>
     </FooterButtonLayout>
   );
