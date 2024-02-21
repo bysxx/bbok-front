e8aaccf92b6543af3be7180f9c53cff399c913f1
suminhan123 <suminh324@gmail.com>
2024-02-21 14:58:20 +0900

    feat: add member query key constants

--- app/common/constants/queryKeys.ts
+++ app/common/constants/queryKeys.ts
@@ -2,3 +2,4 @@ import { createQueryKeys } from '@libs/queryKeys';
 
 export const CHECKLIST_KEYS = createQueryKeys('checklist');
 export const FRIEND_KEYS = createQueryKeys('friend');
+export const MEMBER_KEYS = createQueryKeys('member');
