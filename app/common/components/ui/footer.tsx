1a972bb738f3cc9809d451257ac768882b3e0953
suminhan123 <suminh324@gmail.com>
2024-02-27 23:53:02 +0900

    feat: modify footer navigate logic

--- app/common/components/ui/footer.tsx
+++ app/common/components/ui/footer.tsx
@@ -1,43 +1,40 @@
 'use client';
 
+import ImageLoader from '@components/imageLoader';
+import { TBottomTab } from '@constants/enums';
+import { BOTTOM_TAP, BOTTOM_TAP_ARRAY } from '@constants/tab';
+import useCustomRouter from '@hooks/useCustomRouter';
 import classNames from 'classnames';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 function Footer() {
-  const pathname = usePathname();
-  const navItems: { href: string; label: string; iconOn: string; iconOff: string }[] = [
-    {
-      href: '/',
-      label: '홈',
-      iconOn: '/images/icon/bottom/home-on.svg',
-      iconOff: '/images/icon/bottom/home-off.svg',
-    },
-    {
-      href: '/diarylist',
-      label: '글쓰기',
-      iconOn: '/images/icon/bottom/writing-on.svg',
-      iconOff: '/images/icon/bottom/writing-off.svg',
-    },
-    {
-      href: '/mypage',
-      label: '마이페이지',
-      iconOn: '/images/icon/bottom/mypage-on.svg',
-      iconOff: '/images/icon/bottom/mypage-off.svg',
-    },
-  ];
+  const { push, pathname } = useCustomRouter();
+
+  const handleBottomRouter = (item: TBottomTab) => {
+    if (!BOTTOM_TAP[item].check.includes(pathname)) {
+      push(BOTTOM_TAP[item].href);
+    }
+  };
 
   return (
     <footer className="sticky bottom-0 grid w-full grid-cols-3 border-t border-t-gray-15 bg-[#fbfbfb] text-center text-gray-20">
-      {navItems.map((item) => (
-        <Link
-          className={classNames('flex flex-col p-2 items-center', { 'text-gray-65': item.href === pathname })}
-          key={item.label}
-          href={item.href}
+      {BOTTOM_TAP_ARRAY.map((item) => (
+        <button
+          className={classNames('flex flex-col p-2 items-center', {
+            'text-gray-65': BOTTOM_TAP[item].check.includes(pathname),
+          })}
+          key={BOTTOM_TAP[item].label}
+          onClick={() => handleBottomRouter(item)}
         >
-          <img className="size-8" src={item.href === pathname ? item.iconOn : item.iconOff} alt="" />
-          <span className="text-xs">{item.label}</span>
-        </Link>
+          <Image
+            loader={ImageLoader}
+            width={36}
+            height={36}
+            src={BOTTOM_TAP[item].check.includes(pathname) ? BOTTOM_TAP[item].iconOn : BOTTOM_TAP[item].iconOff}
+            alt=""
+          />
+          <span className="text-xs">{BOTTOM_TAP[item].label}</span>
+        </button>
       ))}
     </footer>
   );
