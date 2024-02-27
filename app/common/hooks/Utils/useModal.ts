a455176b2022d9bfe5881c89df3e9ac879a3a1af
suminhan123 <suminh324@gmail.com>
2024-02-28 00:32:47 +0900

    feat: add useModal hook

---
+++ app/common/hooks/Utils/useModal.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const useModal = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onOpen = () => {
+    setIsOpen(true);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  return {
+    isOpen,
+    onOpen,
+    onClose,
+  };
+};
+export default useModal;
