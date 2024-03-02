8c9c295282c28aa66033ff63fe554f8a179ea53e
suminhan123 <suminh324@gmail.com>
2024-03-02 14:42:34 +0900

    refactor: refactor diarylist page

--- app/diarylist/page.tsx
+++ app/diarylist/page.tsx
@@ -1,57 +1,6 @@
-'use client';
-
-import BoxButton from '@components/buttons/box-button';
-import SearchBar from '@components/search-bar';
-import useCustomRouter from '@hooks/useCustomRouter';
-import type { ChangeEvent } from 'react';
-import { useCallback, useState } from 'react';
-
-import DiaryList from './diary-list';
-import TagButtonsList from './tag-buttons-list';
+import { EmptyDiaryListPage } from '@features/diary/page';
 
 const DiaryListPage = () => {
-  const { push } = useCustomRouter();
-  // 검색 string 옵션
-  const [input, setInput] = useState<string>('');
-
-  // 카테고리 옵션
-  const [tag, setTag] = useState<string[]>([]);
-  const handleSetTag = useCallback(
-    (value: string[]) => {
-      setTag(value);
-    },
-    [setTag],
-  );
-
-  // 시간 순 옵션
-  const [time, setTime] = useState<string>('desc');
-  const handleSetTime = useCallback(
-    (event: ChangeEvent<HTMLSelectElement>) => {
-      setTime(event.target.value);
-    },
-    [setTime],
-  );
-
-  return (
-    <div className="flex h-full flex-col">
-      <div className="flex flex-1 flex-col">
-        <div className="py-[10px]">
-          <SearchBar input={input} setInput={setInput} onClick={() => {}} href="./" />
-        </div>
-        <TagButtonsList selectTag={tag} setSelectTag={handleSetTag} />
-
-        <DiaryList time={time} setTime={handleSetTime} input={input} tags={tag} />
-      </div>
-      <div className="sticky bottom-0 mt-24 h-[90px]">
-        <BoxButton
-          text="일화작성"
-          border={true}
-          onClick={() => {
-            push({ pathname: './writing', query: { step: 1 } });
-          }}
-        />
-      </div>
-    </div>
-  );
+  return <EmptyDiaryListPage />;
 };
 export default DiaryListPage;
---
+++ app/features/diary/page/index.ts
@@ -0,0 +1,2 @@
+export { default as EmptyDiaryListPage } from './empty-diarylist-page';
+export { default as NonemptyDiaryListPage } from './nonempty-diarylist-page';
