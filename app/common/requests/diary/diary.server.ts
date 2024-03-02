c1035fd201d2feabc0c1429b83b170f2477459d5
suminhan123 <suminh324@gmail.com>
2024-03-02 14:05:19 +0900

    feat: add diary list api at server side

---
+++ app/common/requests/diary/diary.server.ts
@@ -0,0 +1,13 @@
+import { IDiaryListRequest, IDiaryListResponse } from '@interfaces/diary';
+import { httpServer } from '@libs/http.server';
+import { ResponseDTO } from '@requests/common';
+
+const diaryServerApi = {
+  list: async (body: IDiaryListRequest) => {
+    const res = await httpServer.get<ResponseDTO<IDiaryListResponse>>(
+      `/friend/${body.id}/diary?offset=${body.offset}&order=${body.order}&q=${body.q}&tag=${body.tag}`,
+    );
+    return res;
+  },
+};
+export default diaryServerApi;
