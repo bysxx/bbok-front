caf4c841ff436c9ca69075e665d80e5d99612ea8
suminhan123 <suminh324@gmail.com>
2024-03-02 14:14:46 +0900

    feat: add diary query key constant

--- app/common/constants/queryKeys.ts
+++ app/common/constants/queryKeys.ts
@@ -3,3 +3,4 @@ import { createQueryKeys } from '@libs/queryKeys';
 export const CHECKLIST_KEYS = createQueryKeys('checklist');
 export const FRIEND_KEYS = createQueryKeys('friend');
 export const MEMBER_KEYS = createQueryKeys('member');
+export const DIARY_KEYS = createQueryKeys('diary');
