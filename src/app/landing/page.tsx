e48fe4b39632658837320467fb09934076ab08bf
suminhan123 <suminh324@gmail.com>
2024-03-13 17:12:06 +0900

    feat: conver landing from img to next Image

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -10,25 +10,21 @@ export default function LandingPage() {
         <LandingSection
           bg="bg-yellow"
           title="가시같은 친구를 등록하고<br />일화를 작성하세요"
-          image="/images/landing/landing-1.png"
+          image="landing/landing-1.png"
         />
 
         <LandingSection
           bg="bg-orange-5"
           title="나만의 친구 기준으로<br />친구와 있었던 일을 체크해요"
-          image="/images/landing/landing-2.png"
+          image="landing/landing-2.png"
         />
 
-        <LandingSection
-          bg="bg-yellow"
-          title="친구와 나의 거리를<br />확인해보세요"
-          image="/images/landing/landing-3.png"
-        />
+        <LandingSection bg="bg-yellow" title="친구와 나의 거리를<br />확인해보세요" image="landing/landing-3.png" />
 
         <LandingSection
           bg="bg-orange-5"
           title="그날의 감정을 기록하고<br />생각을 정리해요"
-          image="/images/landing/landing-4.png"
+          image="landing/landing-4.png"
         />
       </div>
 
