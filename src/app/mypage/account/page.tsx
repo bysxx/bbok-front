0f8539b817d302480b8ad7ec2880ac8dd911cedc
suminhan123 <suminh324@gmail.com>
2024-03-13 23:16:27 +0900

    design: design my account page

--- src/app/mypage/account/page.tsx
+++ src/app/mypage/account/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import Tab from '@components/tab';
 import { NavTopBar } from '@components/top-bar';
+import { ACCOUNT_TAB_LIST } from '@features/mypage/constants';
 import useCustomRouter from '@hooks/useCustomRouter';
 
 const MyPageAccountPage = () => {
@@ -12,7 +14,11 @@ const MyPageAccountPage = () => {
           push('/mypage');
         }}
         label="내 계정"
+        className="mb-[10px]"
       />
+      {ACCOUNT_TAB_LIST.map((tap, i) => (
+        <Tab key={tap.value} divider={i !== ACCOUNT_TAB_LIST.length - 1} onClick={() => {}} label={tap.label} />
+      ))}
     </div>
   );
 };
