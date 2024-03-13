84622c9b071f65619fbb206cfd67ac7ddb5f7ce3
suminhan123 <suminh324@gmail.com>
2024-03-13 23:16:11 +0900

    feat: apply div type to tob bar component

--- src/common/components/top-bar/nav-top-bar.tsx
+++ src/common/components/top-bar/nav-top-bar.tsx
@@ -1,12 +1,15 @@
-interface NavTopBarInterface {
+import { HTMLAttributes } from 'react';
+import cx from 'classnames';
+
+interface NavTopBarInterface extends HTMLAttributes<HTMLDivElement> {
   label: string;
   onClick: () => void;
 }
 
-const NavTopBar = ({ label, onClick }: NavTopBarInterface) => {
+const NavTopBar = ({ label, onClick, className }: NavTopBarInterface) => {
   return (
     <div className="flex w-full items-center p-4">
-      <img src={'/images/icon/ui/back.svg'} alt="" className="cursor-pointer" onClick={onClick} />
+      <img src={'/images/icon/ui/back.svg'} alt="" className={cx('cursor-pointer', className)} onClick={onClick} />
       <h5 className="mr-5 grow text-center text-lg font-medium text-gray-70">{label}</h5>
     </div>
   );
