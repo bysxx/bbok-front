5fb4e82a07014ad65fc1ff8c54f0ff21c96d680f
suminhan123 <suminh324@gmail.com>
2024-03-14 19:37:44 +0900

    feat: add detail option enum

---
+++ src/common/interfaces/enums/detail.ts
@@ -0,0 +1,7 @@
+export const enum DetailOption {
+  'sticker' = 'sticker',
+  'modify' = 'modify',
+  'criteria' = 'criteria',
+  'delete' = 'delete',
+}
+export type TDetailOption = keyof typeof DetailOption;
--- src/common/interfaces/enums/index.ts
+++ src/common/interfaces/enums/index.ts
@@ -1,6 +1,7 @@
 export * from './account';
 export * from './character';
 export * from './date';
+export * from './detail';
 export * from './emoji';
 export * from './mypage';
 export * from './tab';
