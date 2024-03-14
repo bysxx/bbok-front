96b5bce3115e19133e5cb31c1e227fb2366aa0d9
suminhan123 <suminh324@gmail.com>
2024-03-14 19:52:58 +0900

    feat: apply api data to detail content

--- src/features/detail/components/tag-list.tsx
+++ src/features/detail/components/tag-list.tsx
@@ -1,12 +1,11 @@
 import TagLabel from '@components/tag-label';
-import { diary } from 'src/app/diarylist/dummy';
 
-const TagList = () => {
+const TagList = ({ tags }: { tags: string[] }) => {
   return (
     <div className="pt-8">
       <h5 className="text-lg font-semibold">태그</h5>
       <div className="mt-3 flex items-center justify-start">
-        {diary.tags.map((tag: any) => (
+        {tags.map((tag: any) => (
           <div className="mr-2" key={tag}>
             <TagLabel label={tag} type="orange" />
           </div>
--- src/features/detail/pages/diary-detail-page.tsx
+++ src/features/detail/pages/diary-detail-page.tsx
@@ -1,25 +1,27 @@
 'use client';
 
 import { NavTopBar } from '@components/top-bar';
+import { useGetDiaryDetail } from '@hooks/queries/diary';
 import useCustomRouter from '@hooks/useCustomRouter';
 import { getTime } from '@libs/getTime';
-import { diary } from 'src/app/diarylist/dummy';
+import Image from 'next/image';
 
 import { DetailContent, TagList } from '../components';
 
 const DiaryDetail = ({ id }: { id: number }) => {
   const { push } = useCustomRouter();
-  const diaryDate = getTime(diary.date);
-  console.log(id);
+  const { data } = useGetDiaryDetail(id);
+  const diaryDate = getTime(data?.data.date!);
+
   return (
     <div className="size-full bg-gray-13">
       <NavTopBar onClick={() => push('./')} label={diaryDate} />
       <div className="mt-4 flex items-center justify-center">
-        <img src="../images/emoji/emoji3.svg" alt="" />
+        <Image src={data?.data.emojiUrl!} width={56} height={56} alt="" />
       </div>
       <div className="mx-6">
-        <DetailContent content={diary.content} id={id} />
-        <TagList />
+        <DetailContent content={data?.data.content!} id={id} />
+        <TagList tags={data?.data.tags!} />
       </div>
     </div>
   );
