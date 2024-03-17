8b86aaa79d9c98b6e1fff576a185cbc25fd4c44c
suminhan123 <suminh324@gmail.com>
2024-03-17 20:10:43 +0900

    design: add distance gage card design

---
+++ src/app/distance/layout.tsx
@@ -0,0 +1,4 @@
+const DiaryDistanceLayout = ({ children }: { children: React.ReactNode }) => {
+  return <main className="flex size-full flex-col bg-orange-5">{children}</main>;
+};
+export default DiaryDistanceLayout;
---
+++ src/features/distance/components/card.tsx
@@ -0,0 +1,18 @@
+import ImageLoader from '@components/imageLoader';
+import FriendProgressBar from '@features/friend/component/progress-bar';
+import Image from 'next/image';
+
+import { DISTANCE_GAGE } from '../constants';
+
+const DistanceCard = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center rounded-3xl bg-white px-7 pb-8 pt-1">
+      <Image className="mb-[18px]" loader={ImageLoader} src={DISTANCE_GAGE.Four} width={206} height={168} alt="" />
+      <FriendProgressBar percent={90} />
+
+      <p className="text-title-3 mt-5 text-gray-65">김도리님은 나와</p>
+      <p className="text-title-2 mt-2 text-orange-1">23m 정도 멀어졌어요</p>
+    </div>
+  );
+};
+export default DistanceCard;
