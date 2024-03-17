4aa1560fbd2f4aadc179f4d2ad378a5781a56412
suminhan123 <suminh324@gmail.com>
2024-03-17 23:25:13 +0900

    feat: add distance page navigation

--- src/features/distance/components/progress-bar.tsx
+++ src/features/distance/components/progress-bar.tsx
@@ -3,47 +3,49 @@
 import { Tooltip } from '@chakra-ui/react';
 import ImageLoader from '@components/imageLoader';
 import Image from 'next/image';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const HEART_WIDTH = 32;
 
 interface IDistanceProgressBarProps {
   percent: number;
 }
+
 const DistanceProgressBar = ({ percent }: IDistanceProgressBarProps) => {
   const fillRef = useRef<HTMLDivElement>(null);
   const imgRef = useRef<HTMLImageElement>(null);
+  const [animationDuration, setAnimationDuration] = useState<number>(1000);
 
   useEffect(() => {
     if (fillRef.current && imgRef.current) {
-      fillRef.current.style.width = `${percent}px`;
-      imgRef.current.style.left = `${percent - HEART_WIDTH / 2}px`;
+      fillRef.current.style.width = `${percent}%`;
+      imgRef.current.style.left = `calc(${percent}% - ${HEART_WIDTH / 2}px)`;
+      setAnimationDuration(1000);
     }
   }, [fillRef, imgRef, percent]);
 
   return (
     <>
       <div className="relative flex w-full flex-col">
-        <div className="h-3 rounded-full bg-[#FFCCAA]">
+        <div className="relative h-3 rounded-full bg-[#FFCCAA]">
           <div
             ref={fillRef}
-            style={{ width: '0px' }}
-            className="absolute left-0 top-0 h-3 rounded-full bg-orange-1 transition-all duration-1000"
+            style={{ width: '0%' }}
+            className={`absolute left-0 top-0 h-full rounded-full bg-orange-1 transition-all${animationDuration}`}
           />
-          {percent > 0 && (
-            <Tooltip hasArrow label="100m" bg="#FF802D" color="white" isOpen={true} placement="top">
-              <Image
-                ref={imgRef}
-                loader={ImageLoader}
-                style={{ left: '0px' }}
-                className="absolute -top-2.5 transition-all duration-1000"
-                src="home/heart.svg"
-                alt=""
-                width={HEART_WIDTH}
-                height={HEART_WIDTH}
-              />
-            </Tooltip>
-          )}
+
+          <Tooltip hasArrow label={`${percent.toString()}m`} bg="#FF802D" color="white" isOpen={true} placement="top">
+            <Image
+              ref={imgRef}
+              loader={ImageLoader}
+              style={{ left: '0%' }}
+              className={`absolute -top-2.5 transition-all${animationDuration}`}
+              src="home/heart.svg"
+              alt=""
+              width={HEART_WIDTH}
+              height={HEART_WIDTH}
+            />
+          </Tooltip>
         </div>
 
         <div className="mt-1 flex items-center justify-between">
@@ -54,4 +56,5 @@ const DistanceProgressBar = ({ percent }: IDistanceProgressBarProps) => {
     </>
   );
 };
+
 export default DistanceProgressBar;
