f921b0502c07159b86ea790e0261f9d96e6ecede
suminhan123 <suminh324@gmail.com>
2024-03-19 12:43:35 +0900

    feat: add validation for text input

--- src/features/writing/components/diary-form.tsx
+++ src/features/writing/components/diary-form.tsx
@@ -1,28 +1,24 @@
 'use client';
 
-import DatePicker from '@components/date-picker';
 import TextField from '@components/text-field';
 import { IDiaryRequestBody } from '@interfaces/diary';
-import moment from 'moment';
 import { useController, useFormContext } from 'react-hook-form';
 
 const WritingDiaryForm = () => {
-  const today = moment().format('YYYY-MM-DD');
   const { register, control } = useFormContext<IDiaryRequestBody>();
-  const { field: dateField } = useController({ name: 'date', control, defaultValue: today });
-  const { field: contentField } = useController({ name: 'content', control });
+  const { field } = useController({ name: 'content', control, defaultValue: '' });
 
   return (
     <>
-      <h2 className="mb-3 mt-8 text-base font-medium text-gray-65">날짜</h2>
-      <DatePicker {...register('date')} date={dateField.value} setDate={dateField.onChange} />
-
       <h2 className="mb-3 mt-8 text-base font-medium text-gray-65">친구 일화</h2>
       <TextField
-        {...register('content')}
-        input={contentField.value}
+        {...register('content', {
+          required: '일화 내용을 입력해주세요',
+          maxLength: { value: 1000, message: '최대 1000자까지 입력이 가능해요' },
+        })}
+        input={field.value}
         placeholder={`힘들었던 일, 슬펐던 일 그 어떤 얘기도 좋아요. ${'\n'}뽁이 생각 정리를 도와줄게요!`}
-        setInput={contentField.onChange}
+        setInput={field.onChange}
         maxLength={1000}
       />
     </>
