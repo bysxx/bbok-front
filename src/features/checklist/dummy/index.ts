13bdedb49285170e2b9a2352b1222b7131cc77f2
suminhan123 <suminh324@gmail.com>
2024-03-20 22:59:41 +0900

    feat: add detail dummy data

---
+++ src/features/checklist/dummy/index.ts
@@ -0,0 +1,28 @@
+export const DATA = {
+  badChecklist: [
+    { id: 269, criteria: '나를 배려하지 않은 친구', isUsed: true },
+    { id: 270, criteria: '신뢰를 잃는 행동을 하는 친구', isUsed: true },
+    { id: 271, criteria: '나의 자존감을 낮추는 친구', isUsed: true },
+    { id: 272, criteria: '유머코드가 맞지 않는 친구', isUsed: true },
+    { id: 273, criteria: '나에게 너무 많이 의존하는 친구', isUsed: false },
+    { id: 274, criteria: '이성에 집착하는 친구', isUsed: false },
+    { id: 275, criteria: '자기 과시와 자랑이 심한 친구', isUsed: false },
+    { id: 276, criteria: '종교적·정치적 가치관이 다른 친구', isUsed: false },
+    { id: 277, criteria: '의사소통 스타일이 맞지 않는 친구', isUsed: false },
+    { id: 278, criteria: '나를 가르치려고 하는 친구', isUsed: false },
+    { id: 279, criteria: '내가 정한 벗어난 친구 유형', isUsed: true },
+  ],
+  goodChecklist: [
+    { id: 258, criteria: '이야기를 잘 듣고 공감해주는 친구', isUsed: true },
+    { id: 259, criteria: '존중하고 배려하는 마음을 가진 친구', isUsed: true },
+    { id: 260, criteria: '관심사가 비슷한 친구', isUsed: true },
+    { id: 261, criteria: '신뢰할 수 있는 친구', isUsed: true },
+    { id: 262, criteria: '긍정적인 친구', isUsed: false },
+    { id: 263, criteria: '성격이 잘 맞는 친구', isUsed: false },
+    { id: 264, criteria: '둘이서 만나도 편한 친구', isUsed: false },
+    { id: 265, criteria: '나를 편견없이 대해주는 친구', isUsed: false },
+    { id: 266, criteria: '배울 점이 많은 친구', isUsed: false },
+    { id: 267, criteria: '내 자존감을 올려주는 친구', isUsed: false },
+    { id: 268, criteria: '내가 정한 적합한 친구 유형', isUsed: true },
+  ],
+};
