39537d651b8e71435f66e57add95bf75653faaac
suminhan123 <suminh324@gmail.com>
2024-03-20 19:55:38 +0900

    refactor: refactor modify checklist page

--- src/app/diarylist/[id]/modify/checklist/page.tsx
+++ src/app/diarylist/[id]/modify/checklist/page.tsx
@@ -1,6 +1,16 @@
-import diaryServerApi from '@apis/diary/diary.server';
-import { PrefetchHydration } from '@components/react-query';
-import { DIARY_KEYS } from '@constants/queryKeys';
+'use client';
+
+import { NavTopBar } from '@components/top-bar';
+import { FooterButtonLayout } from '@components/ui/layout';
+import ChecklistTab from '@features/checklist/components/tab';
+import { CHECKLIST_TABS, DIARY_CRITERIA_TEXT } from '@features/checklist/constants';
+import { getDiaryChecklist } from '@features/diary/utils/get-diary-checklist';
+import { useDiaryMutation, useGetDiaryDetail } from '@hooks/queries/diary';
+import useCustomRouter from '@hooks/useCustomRouter';
+import { useTabs } from '@hooks/useTabs';
+import { ICheckItem } from '@interfaces/checklist';
+import { TQuery, TypeQuery } from '@interfaces/enums';
+import { useEffect, useState } from 'react';
 
 interface IDiaryDetailCriteriaProp {
   params: {
@@ -8,11 +18,71 @@ interface IDiaryDetailCriteriaProp {
   };
 }
 
-const DiaryDetailCriteriaPage = ({ params }: IDiaryDetailCriteriaProp) => {
+const DiaryDetailCriteriaPage = ({ params: { id } }: IDiaryDetailCriteriaProp) => {
+  const { patchDiary } = useDiaryMutation();
+  const { back, push } = useCustomRouter();
+  const { data, isSuccess } = useGetDiaryDetail(id);
+  const { currentItem, changeItem } = useTabs<TQuery>(0, CHECKLIST_TABS);
+  const { tab } = currentItem;
+  const [goodChecklist, setGoodChecklist] = useState<ICheckItem[]>([]);
+  const [badChecklist, setBadChecklist] = useState<ICheckItem[]>([]);
+
+  const handleModifyCriteria = () => {
+    const body = {
+      sticker: '',
+      emoji: data?.data.emoji!,
+      date: data?.data.date!,
+      content: data?.data.content!,
+      tags: data?.data.tags!,
+      checklist: getDiaryChecklist(badChecklist, goodChecklist),
+    };
+    patchDiary.mutate({ ...body, id });
+    push(`/diarylist/${id}`);
+  };
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      setBadChecklist(data.data.badChecklist);
+      setGoodChecklist(data.data.goodChecklist);
+    }
+  }, [isSuccess, data]);
+
+  const modifyChecklist: Record<
+    TQuery,
+    { list: ICheckItem[]; setList: (value: ICheckItem[]) => void; onClick: () => void; navClick: () => void }
+  > = {
+    [TypeQuery.good]: {
+      list: goodChecklist,
+      setList: setGoodChecklist,
+      onClick: () => {
+        handleModifyCriteria();
+      },
+      navClick: () => changeItem(0),
+    },
+    [TypeQuery.bad]: {
+      list: badChecklist,
+      setList: setBadChecklist,
+      onClick: () => {
+        changeItem(1);
+      },
+      navClick: () => back(),
+    },
+  };
+
   return (
-    <PrefetchHydration queryKey={DIARY_KEYS.detail([{ ...params }])} queryFn={() => diaryServerApi.detail(params.id)}>
-      {/* <DiaryChecklistModifyPage id={params.id} /> */}
-    </PrefetchHydration>
+    <FooterButtonLayout onClick={modifyChecklist[tab].onClick} text={DIARY_CRITERIA_TEXT[tab].text}>
+      <NavTopBar label="선택 기준 수정" onClick={modifyChecklist[tab].navClick} />
+      <h2 className="px-8 pt-7 text-[17px] font-medium text-gray-70">{DIARY_CRITERIA_TEXT[tab].label}</h2>
+
+      <div className="mt-[34px]">
+        <ChecklistTab
+          type={tab}
+          allList={modifyChecklist[tab].list}
+          setAllList={modifyChecklist[tab].setList}
+          length={modifyChecklist[tab].list.length}
+        />
+      </div>
+    </FooterButtonLayout>
   );
 };
 export default DiaryDetailCriteriaPage;
--- src/features/checklist/components/diary/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as DiaryChecklistModifyPage } from './page';
