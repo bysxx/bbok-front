c43fc8c667ffc9928ec141171a7b4ab5b54b61cd
suminhan123 <suminh324@gmail.com>
2024-03-20 19:21:50 +0900

    feat: update tag modify state using react-hook-form

---
+++ src/app/diarylist/[id]/modify/tag/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import WritingTagForm from '@features/diary/components/writing/tag-form';
+import { useGetDiaryDetail } from '@hooks/queries/diary';
+
+interface ITagsDetailModifyProp {
+  params: {
+    id: number;
+  };
+}
+
+const TagsDetailModifyPage = ({ params }: ITagsDetailModifyProp) => {
+  const { data } = useGetDiaryDetail(params.id);
+  return <WritingTagForm defaultValue={data?.data.tags!} />;
+};
+export default TagsDetailModifyPage;
