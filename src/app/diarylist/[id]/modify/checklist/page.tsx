ffb6cb9ddd512368003a5d0eb262abcd883970a6
suminhan123 <suminh324@gmail.com>
2024-03-20 13:41:00 +0900

    refactor: separate diary modify page into checklist page and diary page

--- src/app/diarylist/[id]/modify/checklist/page.tsx
+++ src/app/diarylist/[id]/modify/checklist/page.tsx
@@ -1,7 +1,6 @@
 import diaryServerApi from '@apis/diary/diary.server';
 import { PrefetchHydration } from '@components/react-query';
 import { DIARY_KEYS } from '@constants/queryKeys';
-import { DiaryChecklistModifyPage } from '@features/checklist/components/diary';
 
 interface IDiaryDetailCriteriaProp {
   params: {
@@ -12,7 +11,7 @@ interface IDiaryDetailCriteriaProp {
 const DiaryDetailCriteriaPage = ({ params }: IDiaryDetailCriteriaProp) => {
   return (
     <PrefetchHydration queryKey={DIARY_KEYS.detail([{ ...params }])} queryFn={() => diaryServerApi.detail(params.id)}>
-      <DiaryChecklistModifyPage id={params.id} />
+      {/* <DiaryChecklistModifyPage id={params.id} /> */}
     </PrefetchHydration>
   );
 };
--- src/features/diary/constants/option.ts
+++ src/features/diary/constants/option.ts
@@ -1,24 +1,25 @@
-import { TDetailOption } from '@interfaces/enums';
+import type { TDetailOption } from '@interfaces/enums';
+import { DetailOption } from '@interfaces/enums';
 
 export const DETAIL_OPTIONS: { label: string; image: string; value: TDetailOption }[] = [
   {
     label: '스티커',
     image: 'icon/ui/sticker.svg',
-    value: 'sticker',
+    value: DetailOption.sticker,
   },
   {
     label: '수정',
     image: 'icon/ui/edit.svg',
-    value: 'modify',
+    value: DetailOption.diary,
   },
   {
     label: '기준',
     image: 'icon/ui/checkbox.svg',
-    value: 'criteria',
+    value: DetailOption.checklist,
   },
   {
     label: '삭제',
     image: 'icon/ui/delete.svg',
-    value: 'delete',
+    value: DetailOption.delete,
   },
 ];
