adae6aec2762dd48b564ab716920b57ef5e4b3f6
suminhan123 <suminh324@gmail.com>
2024-03-23 11:31:21 +0900

    feat: add checklist modify page layout

--- src/app/checklist/modify/layout.tsx
+++ src/app/checklist/modify/layout.tsx
@@ -1,9 +1,22 @@
 'use client';
 
+import { NavTopBar } from '@components/top-bar';
 import { ModifyChecklistFormProvider } from '@features/checklist/contexts';
+import useCustomRouter from '@hooks/useCustomRouter';
 import { PropsWithChildren } from 'react';
 
 const ModifyChecklistLayout = ({ children }: PropsWithChildren) => {
-  return <ModifyChecklistFormProvider>{children}</ModifyChecklistFormProvider>;
+  const { push } = useCustomRouter();
+  return (
+    <ModifyChecklistFormProvider>
+      <NavTopBar
+        label="기준 수정"
+        onClick={() => {
+          push('/checklist/detail');
+        }}
+      />
+      {children}
+    </ModifyChecklistFormProvider>
+  );
 };
 export default ModifyChecklistLayout;
