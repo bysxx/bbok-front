b072fe01008130ff3ad1ed8600d4016016167875
suminhan123 <suminh324@gmail.com>
2024-03-23 11:57:39 +0900

    fix: fix modiy checklist context type

--- src/features/checklist/types/modify-checklist.ts
+++ src/features/checklist/types/modify-checklist.ts
@@ -4,8 +4,8 @@ import { IUserChecklistItem } from '@interfaces/checklist';
  * 체크리스트 수정할 때 context type
  */
 export interface IModifyChecklistContext {
-  addedBadChecklist: IUserChecklistItem<string>;
-  addedGoodChecklist: IUserChecklistItem<string>;
-  modifiedBadChecklis: IUserChecklistItem;
-  modifiedGoodChecklist: IUserChecklistItem;
+  addedBadChecklist: IUserChecklistItem<string>[];
+  addedGoodChecklist: IUserChecklistItem<string>[];
+  modifiedBadChecklis: IUserChecklistItem[];
+  modifiedGoodChecklist: IUserChecklistItem[];
 }
