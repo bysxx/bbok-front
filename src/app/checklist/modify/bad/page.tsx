4e0ed16b9e27a5889c2c626c18f96e0595e62381
suminhan123 <suminh324@gmail.com>
2024-03-23 11:32:00 +0900

    feat: create checklist modify page about good type and bad type

---
+++ src/app/checklist/modify/bad/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { DefaultLayout, FooterButtonLayout } from '@components/ui/layout';
+import { ChecklistCount } from '@features/checklist/components/create';
+import { DIARY_CRITERIA_TEXT } from '@features/checklist/constants';
+import { DATA } from '@features/checklist/dummy';
+import { TypeQuery } from '@interfaces/enums';
+
+const ModifyBadChecklistPage = () => {
+  // const { data } = useGetMyChecklist();
+  return (
+    <FooterButtonLayout text="완료" onClick={() => {}}>
+      <DefaultLayout className="px-[33px]">
+        <div className="mb-[34px] mt-7 flex items-center justify-between">
+          <h1 className="text-title-3 text-gray-70">{DIARY_CRITERIA_TEXT[TypeQuery.bad].label}</h1>
+          <ChecklistCount list={DATA.badChecklist} />
+        </div>
+
+        <h5 className="text-body-3 mb-4">{DIARY_CRITERIA_TEXT[TypeQuery.bad].label}</h5>
+
+        <form></form>
+      </DefaultLayout>
+    </FooterButtonLayout>
+  );
+};
+export default ModifyBadChecklistPage;
