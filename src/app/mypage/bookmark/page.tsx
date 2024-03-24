80b326392cb2927be8d8048eb2634f97abbfabff
suminhan123 <suminh324@gmail.com>
2024-03-24 13:05:30 +0900

    feat: add prefetch hydration ssr to bookmark list api

--- src/app/mypage/bookmark/page.tsx
+++ src/app/mypage/bookmark/page.tsx
@@ -1,6 +1,13 @@
+import bookmarkServerApi from '@apis/bookmark/bookmark.server';
+import { PrefetchHydration } from '@components/react-query';
+import { BOOKMARK_KEYS } from '@constants/queryKeys';
 import { BookmarkPage } from '@features/mypage/components/bookmark';
 
 const MyPageBookmarkPage = () => {
-  return <BookmarkPage />;
+  return (
+    <PrefetchHydration queryKey={BOOKMARK_KEYS.lists()} queryFn={bookmarkServerApi.get}>
+      <BookmarkPage />
+    </PrefetchHydration>
+  );
 };
 export default MyPageBookmarkPage;
