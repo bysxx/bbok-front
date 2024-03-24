31e6ea50c27d0ec21cba800a8c0afaf4f21b9186
suminhan123 <suminh324@gmail.com>
2024-03-24 12:10:18 +0900

    feat: apply zustand saying store to distance page data

--- src/app/writing/distance/page.tsx
+++ src/app/writing/distance/page.tsx
@@ -3,12 +3,9 @@
 import { ButtonTopBar } from '@components/top-bar';
 import { DistanceBookmark, DistanceCard } from '@features/distance/components';
 import useCustomRouter from '@hooks/useCustomRouter';
-import { useSayingStore } from '@stores/useSayingStore';
 
 const DiaryDistancePage = () => {
   const { push } = useCustomRouter();
-  const { saying } = useSayingStore();
-  console.log(saying);
   return (
     <div className="relative flex flex-1 flex-col">
       <ButtonTopBar
--- src/features/distance/components/bookmark.tsx
+++ src/features/distance/components/bookmark.tsx
@@ -1,15 +1,17 @@
 import { Tooltip } from '@chakra-ui/react';
 import ImageLoader from '@components/imageLoader';
 import useBackgroundFadeIn from '@hooks/useBackgroundFadeIn';
+import { useSayingStore } from '@stores/useSayingStore';
 import Image from 'next/image';
 
 const DistanceBookmark = () => {
   const { ref } = useBackgroundFadeIn(3);
+  const { saying } = useSayingStore();
 
   return (
     <div
       ref={ref}
-      className="relative flex translate-y-0 animate-[bottom-sheet-up_700ms_ease-in-out] flex-col items-center justify-center rounded-xl bg-brownModal px-12 pb-5 pt-[34px] opacity-10 transition-transform duration-500"
+      className="relative flex w-full translate-y-0 animate-[bottom-sheet-up_700ms_ease-in-out] flex-col items-center justify-center rounded-xl bg-brownModal px-12 pb-5 pt-[34px] opacity-10 transition-transform duration-500"
     >
       <div className="absolute right-[10px] top-[10px] cursor-pointer">
         <Tooltip
@@ -24,10 +26,8 @@ const DistanceBookmark = () => {
         </Tooltip>
       </div>
 
-      <p className="text-center text-sm font-medium text-gray-5">
-        궁극적으로 결혼이든 우정이든 관계에서 유대감을 형성하는 것은 대화다.
-      </p>
-      <p className="mt-[23px] text-xs font-medium text-gray-5">아일랜드 작가, 오스카 와일드</p>
+      <p className="text-center text-sm font-medium text-gray-5">{saying.saying.contents}</p>
+      <p className="mt-[23px] text-xs font-medium text-gray-5">{saying.saying.reference}</p>
     </div>
   );
 };
