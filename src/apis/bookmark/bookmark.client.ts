ae9837e8b79ecef51fee3878fff12b764c0bd9d4
suminhan123 <suminh324@gmail.com>
2024-03-24 12:20:59 +0900

    feat: add bookmark post api func

--- src/apis/bookmark/bookmark.client.ts
+++ src/apis/bookmark/bookmark.client.ts
@@ -1,5 +1,5 @@
 import type { IBookmarkListResponse } from '@interfaces/bookmark';
-import type { ResponseDTO } from '@interfaces/common';
+import type { ResponseAPI, ResponseDTO } from '@interfaces/common';
 import { http } from '@libs/http.client';
 
 const bookmarkApi = {
@@ -7,5 +7,10 @@ const bookmarkApi = {
    * @description 북마크 리스트 받아오는 api 함수
    */
   get: async () => http.get<ResponseDTO<IBookmarkListResponse>>('/bookmark'),
+
+  /**
+   * @description 북마크 추가하는 api 함수
+   */
+  post: async (id: number) => http.post<ResponseAPI>('/bookmark', { id }),
 };
 export default bookmarkApi;
