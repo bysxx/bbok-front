0ce3271ba85bb821dd7e872fe05249d96360fd4d
suminhan123 <suminh324@gmail.com>
2024-03-24 13:06:49 +0900

    feat: apply bookmark api response to mypage bookmark

--- src/features/mypage/components/bookmark/card.tsx
+++ src/features/mypage/components/bookmark/card.tsx
@@ -1,22 +1,20 @@
 import ImageLoader from '@components/imageLoader';
+import type { BookmarkContent } from '@interfaces/bookmark';
 import Image from 'next/image';
 
-const BookmarkCard = () => {
+const BookmarkCard = (data: BookmarkContent) => {
   return (
     <div className="relative flex h-32 flex-col items-center justify-center rounded-xl bg-gray-10 px-12 pb-5 pt-[34px] shadow-bookmark">
-      <p className="text-center text-sm font-medium text-gray-65">
-        궁극적으로 결혼이든 우정이든 관계에서 유대감을 형성하는 것은 대화다.
-      </p>
+      <p className="text-center text-sm font-medium text-gray-65">{data.contents}</p>
       <Image
         className="absolute right-[10px] top-[10px]"
         loader={ImageLoader}
-        // src={'icon/ui/bookmark.svg'}
         src={'icon/ui/selected-bookmark.svg'}
         width={24}
         height={24}
         alt=""
       />
-      <p className="mt-[23px] text-xs font-medium text-gray-30">아일랜드 작가, 오스카 와일드</p>
+      <p className="mt-[23px] text-xs font-medium text-gray-30">{data.reference}</p>
     </div>
   );
 };
--- src/features/mypage/components/bookmark/page.tsx
+++ src/features/mypage/components/bookmark/page.tsx
@@ -1,16 +1,24 @@
 'use client';
 
 import { NavTopBar } from '@components/top-bar';
+import { useGetBookmarkList } from '@hooks/queries/bookmark';
+import type { BookmarkContent } from '@interfaces/bookmark';
 
 import BookmarkCard from './card';
+import EmptyBookmarkCard from './empty-card';
 
 const BookmarkPage = () => {
+  const { data } = useGetBookmarkList();
   return (
     <>
       <NavTopBar label="북마크" onClick={() => {}} />
-
-      {/* <EmptyBookmarCard /> */}
-      <BookmarkCard />
+      {data?.data.bookmarks.length === 0 && <EmptyBookmarkCard />}
+      {(data?.data?.bookmarks?.length || 0) > 0 &&
+        data?.data.bookmarks.map((bookmark: BookmarkContent) => (
+          <div key={bookmark.id} className="mt-3 px-4">
+            <BookmarkCard key={bookmark.id} {...bookmark} />
+          </div>
+        ))}
     </>
   );
 };
