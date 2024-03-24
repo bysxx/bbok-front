ca790077f4f2a66fa26da8f29822e6d3eec97bf4
suminhan123 <suminh324@gmail.com>
2024-03-24 11:15:56 +0900

    feat: add bookmark query key constant

--- src/common/constants/queryKeys.ts
+++ src/common/constants/queryKeys.ts
@@ -4,3 +4,4 @@ export const CHECKLIST_KEYS = createQueryKeys('checklist');
 export const FRIEND_KEYS = createQueryKeys('friend');
 export const MEMBER_KEYS = createQueryKeys('member');
 export const DIARY_KEYS = createQueryKeys('diary');
+export const BOOKMARK_KEYS = createQueryKeys('bookmark');
