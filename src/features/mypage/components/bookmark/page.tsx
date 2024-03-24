31629bbb6554973ab334079f78cc52ef74f26f46
suminhan123 <suminh324@gmail.com>
2024-03-24 13:12:49 +0900

    feat: add navigate bookmark to mypage

--- src/features/mypage/components/bookmark/page.tsx
+++ src/features/mypage/components/bookmark/page.tsx
@@ -2,6 +2,7 @@
 
 import { NavTopBar } from '@components/top-bar';
 import { useGetBookmarkList } from '@hooks/queries/bookmark';
+import useCustomRouter from '@hooks/useCustomRouter';
 import type { BookmarkContent } from '@interfaces/bookmark';
 
 import BookmarkCard from './card';
@@ -9,9 +10,15 @@ import EmptyBookmarkCard from './empty-card';
 
 const BookmarkPage = () => {
   const { data } = useGetBookmarkList();
+  const { push } = useCustomRouter();
   return (
     <>
-      <NavTopBar label="북마크" onClick={() => {}} />
+      <NavTopBar
+        label="북마크"
+        onClick={() => {
+          push('./');
+        }}
+      />
       {data?.data.bookmarks.length === 0 && <EmptyBookmarkCard />}
       {(data?.data?.bookmarks?.length || 0) > 0 &&
         data?.data.bookmarks.map((bookmark: BookmarkContent) => (
