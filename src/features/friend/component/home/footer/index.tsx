c5f15ace06d457ad7a70ddd87d7394b2cc9bb0ce
suminhan123 <suminh324@gmail.com>
2024-03-26 13:42:17 +0900

    feat: add home diary tab information tooltip component

---
+++ src/features/friend/component/home/footer/index.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Tooltip } from '@chakra-ui/react';
+import Dimmer from '@components/dimmer';
+import { BOTTOM_TAP_ARRAY } from '@constants/tab';
+import { useCheckTooltipStore } from '@stores/useCheckTooltipStore';
+
+import FooterSection from './section';
+
+const TooltipFooter = () => {
+  const { isCheckDiary, setIsCheckDiary } = useCheckTooltipStore();
+  return (
+    <>
+      <Dimmer isShow={!isCheckDiary} onClose={() => setIsCheckDiary(true)} />
+      <footer className="bottom-0 z-[200] grid w-full grid-cols-3 border-t border-t-gray-15 bg-[#fbfbfb] text-center text-gray-20">
+        {BOTTOM_TAP_ARRAY.map((item) => (
+          <Tooltip
+            key={item}
+            hasArrow
+            label={'친구와의 일화를 써보세요'}
+            bg="#FF802D"
+            color="white"
+            isOpen={!isCheckDiary && item === 'Diary'}
+            placement="top"
+            paddingY={3}
+            paddingX={6}
+            borderRadius={8}
+          >
+            <FooterSection key={item} item={item} />
+          </Tooltip>
+        ))}
+      </footer>
+    </>
+  );
+};
+export default TooltipFooter;
