6b140253ed15fdca7595ab076b363bbd9f862843
suminhan123 <suminh324@gmail.com>
2024-03-26 13:08:34 +0900

    feat: add friend tooltip home page using tooltip portal

--- src/features/friend/component/home/cards/empty-card.tsx
+++ src/features/friend/component/home/cards/empty-card.tsx
@@ -1,8 +1,16 @@
 import { RoundButton } from '@components/buttons';
+import TooltipPortal from '@components/tooltip-portal';
 import useCustomRouter from '@hooks/useCustomRouter';
+import { useCheckTooltipStore } from '@stores/useCheckTooltipStore';
 
 const EmptyFriendCard = () => {
   const { push } = useCustomRouter();
+  const { isCheckFriend, setIsCheckFriend, setIsCheckDiary } = useCheckTooltipStore();
+
+  const onCloseTooltip = () => {
+    setIsCheckFriend(true);
+    setIsCheckDiary(false);
+  };
   return (
     <div className="rounded-3xl bg-white pb-[53px] pt-[51px]">
       <div className="flex flex-col items-center justify-center">
@@ -12,14 +20,19 @@ const EmptyFriendCard = () => {
           친구를 등록하고 일화를 작성하여
         </h5>
         <h5 className="text-caption-1 mb-[17px] text-gray-40">생각을 정리해 보세요</h5>
-
-        <RoundButton
-          type="secondary"
-          onClick={() => {
-            push('/friend/create');
-          }}
-          label="친구 생성"
-        />
+        <TooltipPortal isShow={!isCheckFriend} onClose={onCloseTooltip} label="먼저 친구를 생성해보세요">
+          <RoundButton
+            type="secondary"
+            onClick={() => {
+              if (!isCheckFriend) {
+                onCloseTooltip();
+                return;
+              }
+              push('/friend/create');
+            }}
+            label="친구 생성"
+          />
+        </TooltipPortal>
       </div>
     </div>
   );
