4c0fc9ee80d11b96274a41b410d54c340de09d3b
suminhan123 <suminh324@gmail.com>
2024-03-27 17:39:12 +0900

    feat: update zustand persist of useSayingStore

--- src/common/stores/useSayingStore.ts
+++ src/common/stores/useSayingStore.ts
@@ -1,20 +1,31 @@
 import type { IPostDiaryResponse } from '@interfaces/diary';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+const StorageKey = 'saying';
 
 interface ISayingStoreState {
   saying: IPostDiaryResponse;
   setSaying: (state: IPostDiaryResponse) => void;
 }
 
-export const useSayingStore = create<ISayingStoreState>((set) => ({
-  saying: {
-    friendPercentage: 0,
-    saying: {
-      contents: '',
-      id: 0,
-      isMarked: false,
-      reference: '',
+export const useSayingStore = create(
+  persist<ISayingStoreState>(
+    (set) => ({
+      saying: {
+        friendPercentage: 0,
+        saying: {
+          contents: '',
+          id: 0,
+          isMarked: false,
+          reference: '',
+        },
+      },
+
+      setSaying: (state: IPostDiaryResponse) => set({ saying: state }),
+    }),
+    {
+      name: StorageKey,
     },
-  },
-  setSaying: (state: IPostDiaryResponse) => set({ saying: state }),
-}));
+  ),
+);
