a60cede65ee49798aa97bcaf61abb55bd234c871
suminhan123 <suminh324@gmail.com>
2024-03-27 19:46:52 +0900

    feat: add localstorage delete lib funcs

---
+++ src/common/libs/local-storage/localStorage.ts
@@ -0,0 +1,18 @@
+import { LocalStorageKey } from './localStorageKey';
+
+/**
+ * 계정 탈퇴했을 때, 로컬 스토리지 값 삭제
+ */
+export const clearLocalStorage = () => {
+  localStorage.removeItem(LocalStorageKey.activeFriend);
+  localStorage.removeItem(LocalStorageKey.homeTooltip);
+  localStorage.removeItem(LocalStorageKey.saying);
+};
+
+/**
+ * 친구 관계 정리할 때, 로컬 스토리지 값 삭제
+ */
+export const clearDeleteFriend = () => {
+  localStorage.removeItem(LocalStorageKey.saying);
+  localStorage.removeItem(LocalStorageKey.activeFriend);
+};
