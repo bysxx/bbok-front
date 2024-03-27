3e419f98e7b74dfe1dd65414fb3ad10aed519279
suminhan123 <suminh324@gmail.com>
2024-03-27 15:04:34 +0900

    feat: add login feature

--- src/features/mypage/pages/my-info-page.tsx
+++ src/features/mypage/pages/my-info-page.tsx
@@ -4,6 +4,8 @@ import Tab from '@components/tab';
 import { useGetMyInfo } from '@hooks/queries/member';
 import useCustomRouter from '@hooks/useCustomRouter';
 import { MypageTab } from '@interfaces/enums';
+import { clearTokens } from '@libs/cookie/manageCookie.client';
+import { showSuccessToast } from '@libs/showToast';
 
 import { MypageLayout, Profile } from '../components';
 import { TAP_LIST } from '../constants';
@@ -16,14 +18,16 @@ const MyInfoPage = () => {
       <div className="pl-7 pt-4">
         <h1 className="text-title-1 text-gray-70">내정보</h1>
       </div>
-      <Profile src={data?.data.profileUrl!!} name={data?.data.memberName!!} />
+      <Profile src={data?.data.profileUrl || ''} name={data?.data.memberName || ''} />
       {TAP_LIST.map((tap, i) => (
         <Tab
           key={tap.value}
           divider={i !== TAP_LIST.length - 1}
           onClick={() => {
             if (tap.value === MypageTab.logout) {
-              // TODO: 로그아웃 api 수행
+              clearTokens();
+              push('/login');
+              showSuccessToast('성공적으로 로그아웃되었어요');
             } else {
               push(`/mypage/${tap.value}`);
             }
