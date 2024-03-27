38ab732250b519ef7e2d62f096098a3add798582
suminhan123 <suminh324@gmail.com>
2024-03-27 15:20:12 +0900

    chore: add react query default option

--- src/common/providers/react-query-provider.tsx
+++ src/common/providers/react-query-provider.tsx
@@ -2,10 +2,18 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import type { PropsWithChildren } from 'react';
 import React from 'react';
 
-function ReactQueryProvider({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient();
+function ReactQueryProvider({ children }: PropsWithChildren) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        gcTime: Infinity,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
