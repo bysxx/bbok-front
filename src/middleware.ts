7edd1dbe4c3913f6c9481d85b53bea5bf6a3481b
suminhan123 <suminh324@gmail.com>
2024-03-28 19:02:33 +0900

    feat: implement redirect to /diarylist when id is a string in /diarylist/[id] to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,27 @@ import { NextResponse } from 'next/server';
 export const middleware = (request: NextRequest) => {
   const accessToken = getAccessToken();
   const isVisited = checkIsVisited();
+
   if (!isVisited) {
     return NextResponse.redirect(new URL(`/landing`, request.nextUrl));
   }
   if (!accessToken) {
     return NextResponse.redirect(new URL(`/login`, request.nextUrl));
   }
+
+  /**
+   * /diarylist/[id] id 값이 문자열 인 경우 /diarylist 로 redirect
+   */
+  if (request.nextUrl.pathname.startsWith('/diarylist')) {
+    const [, , id] = request.nextUrl.pathname.split('/');
+    const isAllNumberString = (string: string) => /^\d+$/.test(string);
+
+    if (id && !isAllNumberString(id)) return NextResponse.redirect(new URL('/diarylist', request.nextUrl));
+  }
+
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: ['/', '/checklist', '/criteria', '/diarylist', '/friend', '/mypage'],
+  matcher: ['/', '/checklist', '/criteria', '/diarylist', '/diarylist/:path+', '/friend', '/mypage'],
 };
