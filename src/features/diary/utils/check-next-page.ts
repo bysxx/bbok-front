ed7faac9d3e0d581288de0dc3092db1e79fb0179
suminhan123 <suminh324@gmail.com>
2024-03-29 16:10:46 +0900

    fix: fix checkNextPage util func in diary writing page

--- src/features/diary/utils/check-next-page.ts
+++ src/features/diary/utils/check-next-page.ts
@@ -1,8 +1,18 @@
-import { IDiaryRequestBody } from '@interfaces/diary';
+import type { TEmoji } from '@interfaces/enums';
 
 /**
  * 일화 다음 페이지로 이동 여부 반환 함수
  */
-export const CheckNotNextPage = (diary: IDiaryRequestBody): boolean => {
-  return diary.tags.length === 0 || !diary.content || !diary.date || !diary.emoji;
+export const CheckNotNextPage = ({
+  tags,
+  content,
+  date,
+  emoji,
+}: {
+  tags: null | string[];
+  content: string;
+  date: string;
+  emoji: TEmoji | null;
+}): boolean => {
+  return !tags || (tags && tags.length === 0) || content.length === 0 || !date || !emoji;
 };
