8db9318333b3024eba88ac866a448fcec76edb62
suminhan123 <suminh324@gmail.com>
2024-03-29 13:26:17 +0900

    refactor: refacotor middleware diarylist route

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,19 +15,14 @@ export const middleware = (request: NextRequest) => {
 
   /**
    * /diarylist/[id] id 값이 문자열이 포함된 경우 /diarylist 로 redirect
-   */
-  if (request.nextUrl.pathname.startsWith('/diarylist')) {
-    const [, , id] = request.nextUrl.pathname.split('/');
-    const isAllNumberString = (string: string) => /^\d+$/.test(string);
-    if (id && !isAllNumberString(id)) return NextResponse.redirect(new URL('/diarylist', request.nextUrl));
-  }
-
-  /**
    * diarylist url param이 더 추가되면 /diarylist 로 route
    */
   if (request.nextUrl.pathname.startsWith('/diarylist')) {
-    const [, , , , , step] = request.nextUrl.pathname.split('/');
-    if (step) return NextResponse.redirect(new URL(`/diarylist`, request.nextUrl));
+    const [, , id, param, , step] = request.nextUrl.pathname.split('/');
+    const isAllNumberString = (string: string) => /^\d+$/.test(string);
+    if (id && !isAllNumberString(id)) return NextResponse.redirect(new URL('/diarylist', request.nextUrl));
+    if ((param && param !== 'modify') || step)
+      return NextResponse.redirect(new URL(`/diarylist/${id}`, request.nextUrl));
   }
 
   /**
