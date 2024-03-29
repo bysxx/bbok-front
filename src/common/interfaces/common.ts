4fd73a35bba606e7f0e7b9077799c64e76d41250
suminhan123 <suminh324@gmail.com>
2024-03-29 19:32:02 +0900

    feat: add common error response api and error code type

--- src/common/interfaces/common.ts
+++ src/common/interfaces/common.ts
@@ -7,14 +7,26 @@ export interface ResponseDTO<T> extends ResponseAPI {
   data: T;
 }
 
+type TErrorCode =
+  | 'D003'
+  | 'E001'
+  | 'C001'
+  | 'C002'
+  | 'C003'
+  | 'C004'
+  | 'C005'
+  | 'J001'
+  | 'J002'
+  | 'R001'
+  | 'R002'
+  | 'S001'
+  | 'I001'
+  | 'E001'
+  | 'E004';
+
 export interface ResponseErrorApi {
-  code: string;
+  code: TErrorCode;
   errors: string[];
   message: string;
   status: number;
-}
-
-// fetch request 함수 fetch 옵션 타입 지정
-export interface IRequestOptions extends RequestInit {
-  query?: string | Record<string, any> | URLSearchParams | string[][];
 }
