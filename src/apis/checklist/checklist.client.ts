bd0a88817ba6396b282aaeced5dd1fe0a01eb025
suminhan123 <suminh324@gmail.com>
2024-04-10 22:08:53 +0900

    fix: fix eslint to checklist api

--- src/apis/checklist/checklist.client.ts
+++ src/apis/checklist/checklist.client.ts
@@ -1,21 +1,21 @@
-import { ICheckList, IChecklistCreateRequestBody, IModifyChecklistRequestBody } from '@interfaces/checklist';
-import { ResponseAPI, ResponseDTO } from '@interfaces/common';
+import type { ICheckList, IChecklistCreateRequestBody, IModifyChecklistRequestBody } from '@interfaces/checklist';
+import type { ResponseAPI, ResponseDTO } from '@interfaces/common';
 import { http } from '@libs/http.client';
 
 const checklistApi = {
   /**
    * @description 서비스에서 제공하는 체크리스트 받아오는 api 함수
    */
-  get: async () => await http.get<ResponseDTO<ICheckList>>('/checklist'),
+  get: async () => http.get<ResponseDTO<ICheckList>>('/checklist'),
 
   /**
    * @description 체크리스트 등록
    */
-  post: async (body: IChecklistCreateRequestBody) => await http.post<ResponseAPI>('/checklist', body),
+  post: async (body: IChecklistCreateRequestBody) => http.post<ResponseAPI>('/checklist', body),
 
   /**
    * @description 체크리스트 수정
    */
-  patch: async (body: IModifyChecklistRequestBody) => await http.patch<ResponseAPI>('/member/checklist', body),
+  patch: async (body: IModifyChecklistRequestBody) => http.patch<ResponseAPI>('/member/checklist', body),
 };
 export default checklistApi;
