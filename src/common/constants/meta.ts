0d130fb06736de2e54d414ff786c4cc9348bffc2
suminhan123 <suminh324@gmail.com>
2024-04-10 22:06:27 +0900

    feat: add meta data for seo

---
+++ src/common/constants/meta.ts
@@ -0,0 +1,7 @@
+export const META = {
+  TITLE: '뽁 | 나를 아프게 하는 가시를 뽁! 친구관계를 위한 나만의 일기장',
+  SITE_NAME: '뽁',
+  DESCRIPTION:
+    '친구와 관계를 정리하고 싶었던 순간이 있으셨나요? 나를 아프게 하는 가시를 뽁! 친구관계를 위한 나만의 일기장, 뽁을 이용해보세요.',
+  DOMAIN_URL: 'https://bbok-sigma.vercel.app',
+} as const;
