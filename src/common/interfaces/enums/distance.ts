e783b2f488f377e8a81ac69d0b0270b8b3911372
suminhan123 <suminh324@gmail.com>
2024-04-11 11:02:59 +0900

    refactor: refactor distance const enum

--- src/common/interfaces/enums/distance.ts
+++
@@ -1,7 +0,0 @@
-export const enum DistancePercent {
-  'One' = 'One',
-  'Two' = 'Two',
-  'Three' = 'Three',
-  'Four' = 'Four',
-}
-export type TDistancePercent = keyof typeof DistancePercent;
--- src/features/distance/constants/distance.ts
+++ src/features/distance/constants/distance.ts
@@ -1,10 +1,9 @@
-import type { TDistancePercent } from '@interfaces/enums';
-import { DistancePercent } from '@interfaces/enums';
+export type TDistancePercent = 'One' | 'Two' | 'Three' | 'Four';
 
 export const DISTANCE_GAGE: Record<TDistancePercent, string> = {
-  [DistancePercent.One]: 'illustration/distance-50less.svg',
-  [DistancePercent.Two]: 'illustration/distance-50over.svg',
-  [DistancePercent.Three]: 'illustration/distance-80over.svg',
-  [DistancePercent.Four]: 'illustration/distance-90over.svg',
-};
+  One: 'illustration/distance-50less.svg',
+  Two: 'illustration/distance-50over.svg',
+  Three: 'illustration/distance-80over.svg',
+  Four: 'illustration/distance-90over.svg',
+} as const;
 export type TDistanceImg = (typeof DISTANCE_GAGE)[keyof typeof DISTANCE_GAGE];
